5462977de5d063e682856112f3b79806a0fc766a
Décio <deciocavalcantedeandrade@gmail.com>
2024-10-07 16:38:35 -0300

    Lista de contatos em ordem alfabética

--- app/services/google_maps_service.rb
+++ app/services/google_maps_service.rb
@@ -1,13 +1,16 @@
 require 'net/http'
 require 'json'
+require 'cgi' # Certifique-se de carregar a biblioteca CGI
 
 class GoogleMapsService
   BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 
   def self.fetch_lat_long(address, api_key)
-    url = URI("#{BASE_URL}?address=#{URI.encode(address)}&key=#{api_key}")
+    encoded_address = CGI.escape(address) # Substitui URI.encode
+    url = URI("#{BASE_URL}?address=#{encoded_address}&key=#{api_key}")
     response = Net::HTTP.get(url)
     result = JSON.parse(response)
+
     if result['status'] == 'OK'
       {
         latitude: result['results'].first['geometry']['location']['lat'],
@@ -16,5 +19,7 @@ class GoogleMapsService
     else
       { error: result['status'] }
     end
+  rescue StandardError => e
+    { error: e.message }
   end
 end
