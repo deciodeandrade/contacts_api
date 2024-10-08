8f1360c010275e31c3c7695762bbba53e76eeb92
Décio <deciocavalcantedeandrade@gmail.com>
2024-10-08 18:30:08 -0300

    Remoção de lixo

--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -1,6 +1,2 @@
 module ControllerSpecHelper
-  # Gera um token JWT para o usuário
-  def token_generator(user_id)
-    JsonWebToken.encode(user_id: user_id)
-  end
 end
--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -1,17 +1,2 @@
 module RequestSpecHelper
-  # Gera headers válidos para autenticação
-  def valid_headers
-    {
-      "Authorization" => token_generator(user.id),  # Gerar o token JWT para o usuário
-      "Content-Type" => "application/json"
-    }
-  end
-
-  # Headers inválidos para teste
-  def invalid_headers
-    {
-      "Authorization" => nil,
-      "Content-Type" => "application/json"
-    }
-  end
 end
